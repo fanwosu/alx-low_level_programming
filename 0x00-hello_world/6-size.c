81a48de1f36fa6babca947a57fdddd3589c0f0e7
Nwosu Friday <fanwosu@gmail.com>
2023-10-05 13:41:13 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,10 +14,10 @@ int main(void)
 	float y;
 	char z;
 
-	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(z));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(z));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(v));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(w));
-	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
-	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(y));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(y));
 	return (0);
 }
