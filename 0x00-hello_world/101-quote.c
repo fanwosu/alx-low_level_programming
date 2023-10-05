0bd0f99df278c7ad0c4ce3995f5369909dc0f89f
Nwosu Friday <fanwosu@gmail.com>
2023-10-05 14:22:43 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
 */
 int main(void)
 {
-	write(STDOUUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char *statementVariable = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fprintf(stderr, "%s", statementVariable);
 	return (1);
 }
