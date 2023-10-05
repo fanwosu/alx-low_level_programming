cb86d077ac283aecff2dae7ae8e1fd764dedcec8
Nwosu Friday <fanwosu@gmail.com>
2023-10-05 12:24:38 -0700

    puts.c

---
+++ 0x00-hello_world/4.puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * Main is the entry point of the function
+ * It used puts instead of print
+ * And it returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
