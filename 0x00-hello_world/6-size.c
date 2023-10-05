0299320af96f4ac88226df2c059e83c8781ed18b
Nwosu Friday <fanwosu@gmail.com>
2023-10-05 13:38:33 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  *
  * type: integer
  *
- * return: 0
+ * Return: returns 0 on success
 */
 int main(void)
 {
@@ -14,10 +14,10 @@ int main(void)
 	float y;
 	char z;
 
+	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(z));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(v));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(w));
 	printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(x));
 	printf("Size of float: %lu byte(s)\n", (unsigned long)sizeof(y));
-	printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(z));
 	return (0);
 }
