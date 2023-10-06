ddd24191880adc8a2bfa64a30f37d23f555669b6
Nwosu Friday <fanwosu@gmail.com>
2023-10-06 07:23:06 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,6 @@
 #include <time.h>
 /** main - entry point 
  *
- * type: returns an in
- *
- * functionality: assigns a random number to n each time it's called
- *
  * Return: on successful, it returns a 0
 */
 int main(void)
@@ -16,11 +12,11 @@ int main(void)
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
 		
-		if(n<0)
+		if(n < 0)
 		{
 			printf("%d is negative\n", n);
 		}
-		else if(n==0)
+		else if(n == 0)
 		{
 			printf("%d is zero\n", n);
 		}
