072b951345adc80d9c0b406c0de0470b86c52fb0
Nwosu Friday <fanwosu@gmail.com>
2023-10-06 08:12:11 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,19 +13,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int lastdigit;
-	lastdigit = n % 10;
+	int last_digit;
+	last_digit = n % 10;
 
-	if (lastdigit == 0)
+	if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d: and is 0\n", n,lastdigit);
+		printf("Last digit of %d is %d: and is 0\n", n,last_digit);
 	}
-	else if (lastdigit < 6)
+	else if (last_digit < 6)
 	{
-		printf("Last digit of %d is %d: and it is less than 6 and not 0\n", n,lastdigit);
+		printf("Last digit of %d is %d: and it is less than 6 and not 0\n", n,last_digit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d: and i is grater than 5\n", n,lastdigit);
+		printf("Last digit of %d is %d: and i is grater than 5\n", n,last_digit);
 	}
 }
