c60777edb4679dec2b7c7b4e4c890e63f8d1d184
Nwosu Friday <fanwosu@gmail.com>
2023-10-06 14:50:07 -0700

    print combo4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - is the entry point of function
+ *
+ * Return: on success, this function returns 0
+*/
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		for (y = '0'; y <= '9'; y++)
+		{
+			if (y > x)
+			{
+				for (z ='0'; z <= '9'; z++)
+				{
+					if (z > y)
+					{
+						putchar(x);
+						putchar(y);
+						putchar(z);
+						if (x < '7')
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
