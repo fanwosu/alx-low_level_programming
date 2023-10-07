f2d3df5f4aab1af85cce3e272c7ef65a2a3b2524
Nwosu Friday <fanwosu@gmail.com>
2023-10-06 18:07:25 -0700

    print comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,11 +22,11 @@ int main(void)
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
-			if (a != 99 || b != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			if (a == 98 && b == 99)
+				continue;
+				
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
