7c4d55d1bededce138ed5141971b7a6f90a5a900
Nwosu Friday <fanwosu@gmail.com>
2023-10-10 15:27:26 -0700

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - this function prints the sign of a number
+ *
+ * @a: the int to be checked
+ *
+ * Return: returns 1 and prints + if a  is greater than zero, prints 0 if 0 and -1 if a is less than zero
+*/
+int print_sign(int a)
+{
+	if (a == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (a < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,3 +4,4 @@ This directory is for functions and nested loops
 #2-print_alphabet_x10.c is a function that prints 10 times the alphabets, in lowercase, followed by a new line
 #3-islower.c is a function that checks for lowercase character
 #4-isalpha.c is a function that checks for alphabetic character
+#5-sign.c is a function that prints the sign of a number
