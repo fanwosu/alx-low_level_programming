093df8618dd690488838575e06dc0655ce1334f9
Nwosu Friday <fanwosu@gmail.com>
2023-10-10 09:55:22 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - the entry point of this function
+ *
+ * Return: returns 0 on success
+*/
+int main(void)
+{
+	int i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 This directory is for functions and nested loops
 #0-putchar.c is a program that prints _putchar, followed by a new line
+#1-alphabet.c is a function that prints the alphabets, in lowercase, followed by a new line
