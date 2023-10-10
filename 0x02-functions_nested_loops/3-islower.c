53494cd95a90a377446d3e42eea2a43e83ca17ef
Nwosu Friday <fanwosu@gmail.com>
2023-10-10 14:43:23 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower -  is a function that checks for lowercase
+ *
+ * Return: returns 1 if lowercase and 0 if not lowercase
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
