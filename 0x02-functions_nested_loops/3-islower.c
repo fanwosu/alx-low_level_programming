cf4d57ca3cdfc4d186efebcd793d336c2f162ce8
Nwosu Friday <fanwosu@gmail.com>
2023-10-10 14:45:47 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,11 @@
 /**
  * _islower -  is a function that checks for lowercase
  *
+ * x: this is the character to check
+ *
  * Return: returns 1 if lowercase and 0 if not lowercase
 */
-int _islower(int c)
+int _islower(int x)
 {
-	return (c >= 'a' && c <= 'z');
+	return (x >= 'a' && x <= 'z');
 }
