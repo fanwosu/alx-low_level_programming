f689c90356f6c9500f7e659e41fd9cb5019f4718
Nwosu Friday <fanwosu@gmail.com>
2023-10-10 10:51:58 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - the entry point of this function
+ *print_alphabet - the entry point of this function
  *
- * Return: returns 0 on success
+ *Return: returns 0 on success
 */
 void print_alphabet(void)
 {
@@ -14,9 +14,4 @@ void print_alphabet(void)
 		i++;
 	}
 	_putchar('\n');
-}
-int main(void)
-{
-	print_alphabet();
-	return(0);
 }
