c5d4bf3f708b9db969f0736c8e1ce0fca8db3a58
Nwosu Friday <fanwosu@gmail.com>
2023-10-10 09:29:07 -0700

    _print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /*
- * main - entry point of function
+ *main - entry point of function
  *
- * Return: returns 0 on success
+ *Return: returns 0 on success
 */
 int main(void)
 {
@@ -15,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	
 	return (0);
 }
