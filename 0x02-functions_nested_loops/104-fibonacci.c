3b2ebcc2db8b4052e4d343c6b0cfef5ba14fed16
Nwosu Friday <fanwosu@gmail.com>
2023-10-11 13:39:40 -0700

    fibonacci 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - this is the function that prints the first 98 fibonacci numbers
+ * Return: returns 98 numbers
+*/
+
+int main(void)
+{
+	long x, first = 1, second = 2, next;
+
+	for (x = 0; x < 98; x++)
+	{
+		if (x == 0)
+		{
+			printf("%ld, ", first);
+		}
+		else
+		{
+			if (second != 20365011074)
+			{
+				printf("%ld, ", second);
+			}
+			else
+			{
+				printf("%ld\n", second);
+				break;
+			}
+			next = first + second;
+			first = second;
+			second = next;
+		}
+	}
+	return (0);
+}
