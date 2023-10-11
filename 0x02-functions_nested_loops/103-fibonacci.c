0b0c0f507561b9edfff526a8a94bb0c9e2851d85
Nwosu Friday <fanwosu@gmail.com>
2023-10-11 13:27:30 -0700

    fibonacci sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - this function finds and prints sum of even fibonacci numbers
+ * Return: Always return 0
+*/
+
+int main(void)
+{
+	int x;
+	unsigned long int first, second, next, sum;
+	
+	first = 1;
+	second = 2;
+	sum = 0;
+	for (x = 1; x <= 33; ++x)
+	{
+		if (first < 4000000 && (first % 2) == 0)
+		{
+			sum = sum + first;
+		}
+		next = first + second;
+		first = second;
+		second = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
