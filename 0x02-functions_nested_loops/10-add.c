c52907f135ab853461a6617af9ac88c593aa0114
Nwosu Friday <fanwosu@gmail.com>
2023-10-10 18:22:42 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add -this is a function that adds 2 numbers
+ *
+ * @x: integer to be added
+ *
+ * @y: second integer to be added
+ *
+ * Return: the sum of a and b
+*/
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
