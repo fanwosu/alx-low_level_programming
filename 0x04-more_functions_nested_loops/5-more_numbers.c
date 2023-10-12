0b74af8db17c571ac3c2c884a96365fe048605db
Nwosu Friday <fanwosu@gmail.com>
2023-10-12 12:12:40 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * more_numbers - prints from 0 - 14 ten timex
- *
- * Return: 0
+ *more_numbers - prints 10 times the numbers, from 0 to 14
+ *followed by a new line
 */
-
 void more_numbers(void)
 {
-	int x = 1;
+	int x, y;
 
-	while (x <= 10)
+	for (x = 0; x < 10; x++)
 	{
-		int i = 0;
-		
-		while (i <= 14)
+		for (y = 0; y < 15; y++)
 		{
-			_putchar(i);
-			i++;
+			if (y >= 10)
+			{
+				_putchar(y / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
 		}
-
 		_putchar('\n');
-		x++;
 	}
 }
