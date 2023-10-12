bf38a46125bf83629d1c850257f1ba6e36ea5017
Nwosu Friday <fanwosu@gmail.com>
2023-10-12 13:20:31 -0700

    draw line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - draws a straight line
+ * @n: is the number of times _ is printed
+*/
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		
+		_putchar('\n');
+	}
+}
