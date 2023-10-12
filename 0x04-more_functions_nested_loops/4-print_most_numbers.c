d7895e31b7dbf74fc309cadbfbdf384c73f205d2
Nwosu Friday <fanwosu@gmail.com>
2023-10-12 10:22:33 -0700

    print most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints nums from 0 - 9
+ * print_most_numbers - prints nums from 0 - 9
  *
  * followed by new line
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != 2 || i != 4)
 		{
 			_putchar(i + '0');
 		}
