6499d7e2cbca8261306c35065e2388dd1d4916bf
Nwosu Friday <fanwosu@gmail.com>
2023-10-12 11:50:41 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints from 0 - 14 ten timex
+ *
+ * Return: 0
+*/
+
+void more_numbers(void)
+{
+	int x;
+
+	while (x <= 10 )
+	{
+		int i;
+		
+		while (i <= 14)
+		{
+			_putchar(i);
+			i++;
+		}
+
+		x++;
+		_putchar('\n');
+	}
+}
