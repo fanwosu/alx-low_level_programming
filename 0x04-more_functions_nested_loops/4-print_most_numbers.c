472ce63d3768058d3dbb82c37977279e0c219537
Nwosu Friday <fanwosu@gmail.com>
2023-10-12 10:34:36 -0700

    print more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	int x;
 
-	for (i = 0; i <= 9; i++)
+	for (x = 0; x <= 9; x++)
 	{
-		if ((i != 2) || (i != 4))
+		if (x != 2 && x != 4)
 		{
-			_putchar(i + '0');
+			_putchar(x + '0');
 		}
 	}
 
