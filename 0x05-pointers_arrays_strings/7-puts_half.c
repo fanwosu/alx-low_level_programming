7042e813dbc6d42e95563f6f4f95626e2183b10b
Nwosu Friday <fanwosu@gmail.com>
2023-10-16 12:35:02 -0700

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,24 +4,25 @@
 /**
  *_puts_half - function that prints a string, followed by a new line, to stdout
  * @string: string to be printed
+ * Return: half of string
  */
 void _puts_half(char *string)
 {
-	int length;
-	int counter = 0;
+	int x, half, length;
+	int length = 0;
 
-	length = strlen(string);
-	while (*string != '\0')
+	for (x = 0; string[0] != '\0'; a++)
 	{
-		if(counter < (length / 2))
-		{
-			_putchar(*string++);
-		}
-		else
-		{
-			break;
-		}
-		counter++;
+		length++;
+	}
+	half = length / 2;
+	if (length % 2 == 1)
+	{
+		half = ((length + 1) / 2);
+	}
+	for (x = half; string[x] != '\0'; x++)
+	{
+		_putchar(string[x]);
 	}
 	_putchar('\n');
 }
