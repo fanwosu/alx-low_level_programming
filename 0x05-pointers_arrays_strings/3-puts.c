79593a5a27920f2588d7e0c93ed0135bddb99795
Nwosu Friday <fanwosu@gmail.com>
2023-10-16 09:55:31 -0700

    print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,10 +2,13 @@
 #include <stdio.h>
 /**
  *_puts - function that prints a string, followed by a new line, to stdout
- * @string: string to print
+ * @string: string to be printed
  */
-void _puts(const char *string)
+void _puts(char *string)
 {
-	printf("%s\n", *string);
-	return (0);
+	while (*string != '\0')
+	{
+		_putchar(*string++);
+	}
+	_putchar('\n');
 }
