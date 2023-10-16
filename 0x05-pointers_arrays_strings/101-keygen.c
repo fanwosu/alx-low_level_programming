8ca3822283c2f2102fe264199415286ed7d3863a
Nwosu Friday <fanwosu@gmail.com>
2023-10-16 15:48:25 -0700

    genkey

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,20 +9,20 @@
 int main(void)
 {
 	int pass[100];
-	int i, sum, n;
+	int x, add, y;
 
-	sum = 0;	
+	add = 0;	
 	srand(time(NULL));
-	for (i = 0; i < 100; i++)
+	for (x = 0; x < 100; x++)
 	{
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((2772 - sum) - '0' < 78)
+		if ((2772 - add) - '0' < 78)
 		{
-			n = 2772 - sum - '0';
-			sum += n;
-			putchar(n + '0');
+			y = 2772 - add - '0';
+			add += y;
+			putchar(y + '0');
 			break;
 		}
 														}
