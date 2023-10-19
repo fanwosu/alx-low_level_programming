676ee4f6e549d1769c873bc82f3e5e5547acb320
Nwosu Friday <fanwosu@gmail.com>
2023-10-19 09:03:32 -0700

    turn toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *string_toupper - changes lowercase letters to uppercase in a string
+ * @string: string to work on
+ * Return: the modified string
+ */
+char *string_toupper(char *string)
+{
+	int x = 0;
+
+	while (string[x] != '\0')
+	{
+		if (string[x] >= 'a' && string[x] <= 'z')
+		{
+			string[x] = string[x] - 32;
+		}
+		x++;
+	}
+	return (string);
+}	
---
+++ 0x06-pointers_arrays_strings/REAME.md
@@ -0,0 +1,1 @@
+#5-string_toupper.c is a program that changes all the lowercase letters of a string to uppercase
