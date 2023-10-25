5073cdfe037152c97751b934a7af4dc973e35eb0
Nwosu Friday <fanwosu@gmail.com>
2023-10-25 11:40:22 -0700

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _strlen_recursion - returns the length of a string
- * @s: is the string 
+ * @s: is the string
  * Return: returns the length of string
  */
 int _strlen_recursion(char *s)
