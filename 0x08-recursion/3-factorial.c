078de502178bdec20b989cd1179aef061ab240b7
Nwosu Friday <fanwosu@gmail.com>
2023-10-25 12:26:24 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - a function that returns the factorial of a number
+ * @n: is the number whose factorial is to be returned
+ * Return: void
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n -1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,3 +2,4 @@
 #0-puts_recursive is a function that prints a string, followed by a new line.
 #1-print_rev_recursion.c is a function that prints a string in reverse
 #2-strlen_recursion.c is a function that returns the length of a string
+#3-factorial.c is a function that returns the factorial of a nmber
