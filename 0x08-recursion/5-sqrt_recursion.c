596e84fc7a61155b16fdce1c2b5552444ca5e7ed
Nwosu Friday <fanwosu@gmail.com>
2023-10-25 13:35:16 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,9 @@
 #include "main.h"
 /**
- * _sqrt_recursion - is a function that returns natural square root of a number
- * it also return -1 if number does not have natural square root.
- * @n: is the number whose square root is to be returned
- * Return: returns a square root.
- */
-/**
- * sqrty - function is used to evaluate _sqrt_recursion
- * @x: similar to n in _sqrt_recursion
- * @y: used to iterate from 1 to n or x;
+ * sqrty - function is used to check square root match
+ * between 2 numbers
+ * @x: is the target square root
+ * @y: used to iterate from 1 to x;
  * Return: returns 1 on success
  */
 int sqrty(int x, int y)
@@ -23,6 +18,12 @@ int sqrty(int x, int y)
 	}
 	return (sqrty(x, y + 1));
 }
+/**
+ * _sqrt_recursion - is a function that returns natural square root of a number
+ *it also return -1 if number does not have natural square root.
+ *@n: is the number whose square root is to be returned
+ *Return: returns a square root.
+*/
 int _sqrt_recursion(int n)
 {
 	return (sqrty(n, 1));
