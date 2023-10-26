a6179f0711a12ac7fa9e5448d0af01dfab923d42
Nwosu Friday <fanwosu@gmail.com>
2023-10-25 17:21:39 -0700

    wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * wildcmp - compares 2 strings and returns 1 if the 2 strings are identical
+ * or 0 if otherwise
+ * @string1: string 1
+ * @string2: string 2
+ * Return: returns 1 or 0 
+ */
+int wildcmp(char *string1, char *string2)
+{
+	if (*string1 == '\0' && *string2 == '\0')
+	{
+		return (1);
+	}
+	if (*string1 == *string2)
+	{
+		return (wildcmp(string1 + 1, string2 + 1));
+	}
+	if (*string2 == '*')
+	{
+		if (*string2 == '*' && *(string2 + 1) != '\0' && *string1 == '\0')
+		{
+			return (0);
+		}
+		if (wildcmp(string1, string2 + 1) || wildcmp(string1 + 1, string2))
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -6,3 +6,4 @@
 #4-pow_recursion.c is a function that returns the value of x raised to te power of y
 #5-sqrt_reursion.c is a function that returns the natural square root of a number
 #100-is-palindrome is  function that returns 1 if a string is a palindrome and 0 if not.
+#101-wildcmp.c is a function that compares two strings and returns 1 if the string can be considered identical, and 0 if not.
