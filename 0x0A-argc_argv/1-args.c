8c251734ca829809f70ba0fba9336fa83b3a9326
Nwosu Friday <fanwosu@gmail.com>
2023-10-27 09:34:01 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - function that prints it's own arc
+ * @argc: argument count
+ * @argv: argument vector
+ * Result: returns 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int x =0;
+
+	while (x < argc)
+	{
+		x++;
+		(*argv)++;
+	}
+	printf("%d\n", x - 1);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,3 @@
 0x0A. C - argc, argv
 #0-whatsmyname.c is a function that prints its name, followed by a new line.
+#1-args.c is a function that prints the number of arguments passed into it
