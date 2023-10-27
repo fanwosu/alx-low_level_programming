c687673c0e8e5b3941df77bb551880155acb1928
Nwosu Friday <fanwosu@gmail.com>
2023-10-27 09:47:01 -0700

    args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,13 +3,12 @@
  * main - function that prints it's own arc
  * @argc: argument count
  * @argv: argument vector
- * Result: returns 0 on success
+ * Return: returns 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int x;
-
-	x = 0;	
+	int x = 0;
+	
 	while (x < argc)
 	{
 		x++;
