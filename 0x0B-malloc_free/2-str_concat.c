0420846c6928140b789e22bcfe0a6055b342202b
Nwosu Friday <fanwosu@gmail.com>
2023-10-31 05:09:37 -0700

    concatenate

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - is a function that concatenates two strings
+ * @str1: string 1
+ * @str2: string 2
+ *
+ * Return: pointer to the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *in;
+	int x, y, w, z;
+
+	x = 0;
+	y = 0;
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	while (s1[x] != '\0')
+	{
+		;
+		x++;
+	}
+	while (s2[y] != '\0')
+	{
+		;
+		y++;
+	}
+	in = malloc((sizeof(*s1) * x) + (sizeof(*s2) * y) + 1);
+	if (in == NULL)
+	{
+		return (NULL);
+	}
+	for (w = 0, z = 0; w < (x + y + 1); w++)
+	{
+		if (w < x)
+		{
+			in[w] = s1[w];
+		}
+		else
+		{
+			in[w] = s2[z++];
+		}
+	}
+	return (in);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 0x0B. C - malloc, free
 0-create_array.c is a function that creates an array of chars and initializes it with a specific char
 1-strdup.c is a function that returns a ponter to a newely allocated space in memory which contains a copy of the string given as a parameter
+#2-str_concat.c is a functiont that concatenates 2 strings
