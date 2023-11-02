084adf6d010f51ec6ed57c153e6cef79fc8ebbd1
Nwosu Friday <fanwosu@gmail.com>
2023-11-02 14:27:34 -0700

    array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_rang - creates an array of integers
+ * @min: minimum value of array
+ * @mx: maximum value of array
+ *
+ * Return: pointer to arry created
+ */
+int *array_range(int min, int max)
+{
+	int *ptr, arr;
+
+	if (min > max)
+		return (NULL);
+	ptr = malloc(sizeof(int) * (max - min + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (arr = 0; min <= max; arr++)
+	{
+		ptr[arr] = min;
+		min++;
+	}
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,3 +1,5 @@
 0x0C. C - More malloc, free
 #0-malloc_checked.c is a function that allocates memory using malloc
 #2-calloc.c is a function that allocates memory for an array using malloc
+
+#3-array_range.c is a function that creates an array of integer
