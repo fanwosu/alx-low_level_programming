b17a7a0690af03fcc73512b19c44ccd882d6d4a3
Nwosu Friday <fanwosu@gmail.com>
2023-11-06 10:27:08 -0800

    struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,10 +8,24 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil)\n"));
-		printf("Age: %f\n", d->age);
-		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Ownwer: (nil)\n"));
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		printf("Age: %.6f\n", d->age);
+		if (d->owner != NULL)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
 	}
 }
