81d98abd140982448649e79ae9a7622b8f13c41b
Nwosu Friday <fanwosu@gmail.com>
2023-11-08 12:39:14 -0800

    opscodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point for function
+ * @argc: argument counter
+ * @agrv: argument vector
+ * Return: array of pointers to args
+ */
+int main(int argc, char *argv[])
+{
+	int (*f)(int, char**), x;
+	int hb;
+
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	f = main;
+	for (x = 0; x < atoi(argv[1]); x++)
+	{
+		hb = *(unsigned char *)(f + x);
+		printf("%.2x", hb);
+		if(x < atoi(argv[1]) - 1)
+		{
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -4,3 +4,4 @@
 #3-op_functions.c contaons the 5 functions listed in the 3-calc.c file
 #3-op_functions.c contaons the 5 functions listed in the 3-calc.c file
 #3-op_functions.c contaons the 5 functions listed in the 3-calc.c file
+#100-main_opcodes.c is a program that points the opscodes of it's own main function
