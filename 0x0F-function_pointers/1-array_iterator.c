01b26387848135aaa5c1e0284780342ba6831abc
Nwosu Friday <fanwosu@gmail.com>
2023-11-08 03:34:26 -0800

    iterate

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,6 +4,8 @@
  * @array: array to be iterated
  * @size: is the size of the array
  * @action: pointer to the function to be used
+ *
+ * Return: void;
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
