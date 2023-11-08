951421a920dbf6f27c91f377ad4ebb16cdb5fdbf
Nwosu Friday <fanwosu@gmail.com>
2023-11-08 03:33:06 -0800

    iterate

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that interates the arrays
+ * @array: array to be iterated
+ * @size: is the size of the array
+ * @action: pointer to the function to be used
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+	if (array != NULL && action != NULL && size >0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,2 +1,3 @@
 0x0F. C - Function pointers
 #0-print_name.c is a function that prints name
+#1-array_iterator.c is a function that executes a function as a parameter on each element of an array
