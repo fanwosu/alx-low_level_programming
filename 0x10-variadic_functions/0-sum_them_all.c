6b4b4b3122f95817e63d1afa96bd1d16853b23de
Nwosu Friday <fanwosu@gmail.com>
2023-11-09 06:02:51 -0800

    sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,5 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 /**
  * sum_them_all - function that returns sum
  * @n: is the format
@@ -11,8 +9,8 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list values;
 	unsigned int x;
-	int result = 0;
 
+	int result = 0;
 	if (n == 0)
 	{
 		return (0);
