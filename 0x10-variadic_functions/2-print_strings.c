171a96663fe4b0f0674ac906753b8c1df1fa293e
Nwosu Friday <fanwosu@gmail.com>
2023-11-09 08:53:37 -0800

    print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_strings - prints strings
+ * @separator: seperates
+ * @n: number of string passed to function
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int counter;
+	char *ptr, *s;
+
+	va_list words;
+	va_start(words, n);
+	for (counter = 0; counter < n; counter++)
+	{
+		if (separator == NULL)
+		{
+			s = "";
+		}
+		else
+		{
+			s = (char *)separator;
+		}
+		ptr = va_arg(words, char*);
+		if (counter == 0)
+		{
+			printf("%s", ptr);
+		}
+		if (ptr == NULL)
+		{
+			printf("(nil)");
+		}
+		if (counter > 0)
+		printf("%s%s", s, ptr);
+	}
+	printf("\n");
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -1,3 +1,4 @@
 0x10. C - Variadic functions
 #0-sum_them_all.c is a function that returns the sum of all it's parameters
 #1-print_number is a function that prints numbers, followed by a new line.
+#2-print_string is a programm that prints strings, followed by new line
