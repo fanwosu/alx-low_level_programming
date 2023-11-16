9af46617cc5c709450b6343738d0e159288e8efb
Nwosu Friday <fanwosu@gmail.com>
2023-11-16 08:08:54 -0800

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * free_list - function that frees list
+ * @head: to be freed
+ *
+ * Return: void
+ */
 void free_list(list_t *head)
 {
 	list_t *current = head;
 	list_t *nxt_node;
-	
+
 	while (current != NULL)
 	{
 		nxt_node = current->next;
