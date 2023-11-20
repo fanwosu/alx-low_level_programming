560b71705fb131b2932577819c1585823f20f48d
Nwosu Friday <fanwosu@gmail.com>
2023-11-20 03:52:00 -0800

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listin - prints the elements of a list
+ * @h: head of node
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t number_of_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		number_of_nodes++;
+	}
+	return (number_of_nodes);
+}
