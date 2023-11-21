036961ac4ab1d5cb909ce580c9b60427e1e9a37b
Nwosu Friday <fanwosu@gmail.com>
2023-11-21 09:32:35 -0800

    add node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->next = *head;
 		*head = newNode;
 		return (newNode);
-	} 
+	}
 	current = *head;
 	counter = 0;
 
