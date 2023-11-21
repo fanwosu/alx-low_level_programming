d5c55b2e098900d106c9023d579dd7dd8cbcf45c
Nwosu Friday <fanwosu@gmail.com>
2023-11-21 03:14:22 -0800

    index node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "listsh"
+/**
+ * get_nodeint_at_index - gets the node at a given index
+ * @head: head pointer to first node
+ * @index: node to get
+ *
+ * Return: return an address
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int x = 0;
+
+	while (head != NULL)
+	{
+		if (x == index)
+		{
+			return (head);
+		}
+		head = head->next;
+		x++;
+	}
+	return (NULL);
+}
