a0300d712a10634eb39d05d7fbce3bcd15aa274e
Nwosu Friday <fanwosu@gmail.com>
2023-11-21 00:39:37 -0800

    free current

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint - frees linked list
+ * @head: head pointr
+ *
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current = head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
+
