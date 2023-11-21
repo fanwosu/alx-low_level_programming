0a4dfa4a52f72e1f90560bb0e62bae0c4402678f
Nwosu Friday <fanwosu@gmail.com>
2023-11-21 02:39:19 -0800

    delete node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -13,8 +13,8 @@ int pop_listint(listint_t **head)
 	if (*head == NULL)
 		return (0);
 	temp = *head;
-	value = temp->n;
-	*head = temp->next;
+	value = (temp)->n;
+	*head = (temp)->next;
 	free(temp);
 	return (value);
 }
