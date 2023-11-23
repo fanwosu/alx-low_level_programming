d52393edb79e542cf3491372227cbd40dab9ab34
Nwosu Friday <fanwosu@gmail.com>
2023-11-23 07:17:16 -0800

    get end

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * get_endianness - retunrs the endian of the processor
+ * Return: 1 for little and 0 for big
+ */
+int get_endianness(void)
+{
+	unsigned int x;
+	return ((int) (((char *)&x)[0]));
+}
